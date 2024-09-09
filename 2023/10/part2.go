fccdb07f5a84e46c0b41b6e494367dbe4c5cbd44
vlatan <33163699+vlatan@users.noreply.github.com>
2024-09-10 01:59:51 +0200

    Compact code

--- 2023/10/part2.go
+++ 2023/10/part2.go
@@ -20,8 +20,9 @@ func castRays(matrix []string, loop Graph) (result int) {
 			}
 			count := 0 // cast the ray diagonally and count the intersections
 			for i := 1; x+i < len(matrix) && y+i < len(matrix[0]); i++ {
+				xi, yi := x+i, y+i
 				// check if the point is on the loop
-				node, ok := loop[Loc{x + i, y + i}]
+				node, ok := loop[Loc{xi, yi}]
 				// point not on the loop, no intersection
 				if !ok {
 					continue
@@ -36,11 +37,12 @@ func castRays(matrix []string, loop Graph) (result int) {
 				}
 				// Check if the ray is grazing the corner on the outside.
 				// If so do NOT count the intersection.
-				left, down := Loc{x + i, y + i - 1}, Loc{x + i + 1, y + i}
-				up, right := Loc{x + i - 1, y + i}, Loc{x + i, y + i + 1}
-				c1 := (n1.loc == left && n2.loc == down) || (n2.loc == left && n1.loc == down)
-				c2 := (n1.loc == up && n2.loc == right) || (n2.loc == up && n1.loc == right)
-				if c1 || c2 {
+				left, down := Loc{xi, yi - 1}, Loc{xi + 1, yi}
+				up, right := Loc{xi - 1, yi}, Loc{xi, yi + 1}
+				if (n1.loc == left && n2.loc == down) ||
+					(n2.loc == left && n1.loc == down) ||
+					(n1.loc == up && n2.loc == right) ||
+					(n2.loc == up && n1.loc == right) {
 					continue
 				}
 				count++
