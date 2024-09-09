d9d819cc0e0242563b19fa39f1dfc35accfa6554
vlatan <33163699+vlatan@users.noreply.github.com>
2024-09-09 19:36:43 +0200

    Remove func

--- 2023/10/part2.go
+++ 2023/10/part2.go
@@ -15,26 +15,27 @@ func Part2() {
 func castRays(matrix []string, loop Graph) (result int) {
 	for x, line := range matrix {
 		for y := range line {
-			if _, onloop, _ := onLoop(Loc{x, y}, loop); onloop {
+			if _, ok := loop[Loc{x, y}]; ok {
 				continue
 			}
 			count := 0 // cast the ray diagonally and count the intersections
 			for i := 1; x+i < len(matrix) && y+i < len(matrix[0]); i++ {
 				// check if the point is on the loop
-				node, onloop, oncorner := onLoop(Loc{x + i, y + i}, loop)
+				node, ok := loop[Loc{x + i, y + i}]
 				// point not on the loop, no intersection
-				if !onloop {
+				if !ok {
 					continue
 				}
-				// point is on the loop but not on a corner of the loop
-				// count the intersection and do nothing else
-				if !oncorner {
+				n1, n2 := node.neighbors[0], node.neighbors[1]
+				// Point is NOT on a corner of the loop.
+				// Count the intersection and do nothing else.
+				dx, dy := abs(n1.loc.x-n2.loc.x), abs(n1.loc.y-n2.loc.y)
+				if !(dx == 1 && dy == 1) {
 					count++
 					continue
 				}
 				// Check if the ray is grazing on the outside of the corner.
 				// If so do NOT count the intersection.
-				n1, n2 := node.neighbors[0], node.neighbors[1]
 				left, down := Loc{x + i, y + i - 1}, Loc{x + i + 1, y + i}
 				up, right := Loc{x + i - 1, y + i}, Loc{x + i, y + i + 1}
 				c1 := (n1.loc == left && n2.loc == down) || (n2.loc == left && n1.loc == down)
@@ -52,20 +53,6 @@ func castRays(matrix []string, loop Graph) (result int) {
 		}
 	}
 	return result
-}
-
-// Check if a point is on the loop and if so if it's on a corner too
-func onLoop(loc Loc, loop Graph) (node *Node, onloop bool, oncorner bool) {
-	node, ok := loop[loc]
-	if !ok {
-		return node, false, false
-	}
-	n1, n2 := node.neighbors[0], node.neighbors[1]
-	dx, dy := abs(n1.loc.x-n2.loc.x), abs(n1.loc.y-n2.loc.y)
-	if dx == 1 && dy == 1 {
-		return node, true, true
-	}
-	return node, true, false
 }
 
 // Absolute value of an integer
