b284221d14a6060e92cb061c948bc68d8a6ca992
vlatan <33163699+vlatan@users.noreply.github.com>
2024-09-09 18:08:25 +0200

    Edit comments and increase readability on D10P2

--- 2023/10/part2.go
+++ 2023/10/part2.go
@@ -10,7 +10,7 @@ func Part2() {
 	fmt.Println(result)
 }
 
-// Count points that are enclosed by the loop
+// Count points enclosed by the loop
 // Cast a reay diagonally for every eligible point on the matrix
 func castRays(matrix []string, loop []*Node) (result int) {
 	for x, line := range matrix {
@@ -18,7 +18,7 @@ func castRays(matrix []string, loop []*Node) (result int) {
 			if _, onloop, _ := onLoop(Loc{x, y}, loop); onloop {
 				continue
 			}
-			count := 0 // shoot the ray diagonally
+			count := 0 // cast the ray diagonally and count the intersections
 			for i := 1; x+i < len(matrix) && y+i < len(matrix[0]); i++ {
 				// check if the point is on the loop
 				node, onloop, oncorner := onLoop(Loc{x + i, y + i}, loop)
@@ -32,20 +32,20 @@ func castRays(matrix []string, loop []*Node) (result int) {
 					count++
 					continue
 				}
-				// This is a corner. Check if the ray is grazing on the outside of the corner.
-				// If the ray is not grazing on the outside of the corner count the intersection.
+				// Check if the ray is grazing on the outside of the corner.
+				// If so do NOT count the intersection.
 				n1, n2 := node.neighbors[0], node.neighbors[1]
 				left, down := Loc{x + i, y + i - 1}, Loc{x + i + 1, y + i}
 				up, right := Loc{x + i - 1, y + i}, Loc{x + i, y + i + 1}
-				if ((n1.loc == left || n1.loc == down) &&
-					(n2.loc == left || n2.loc == down)) ||
-					((n1.loc == up || n1.loc == right) &&
-						(n2.loc == up || n2.loc == right)) {
+				c1 := (n1.loc == left && n2.loc == down) || (n2.loc == left && n1.loc == down)
+				c2 := (n1.loc == up && n2.loc == right) || (n2.loc == up && n1.loc == right)
+				if c1 || c2 {
 					continue
 				}
 				count++
 			}
-			// Odd interesections means the point is enclosed by the loop
+			// Odd number of interesections means
+			// the point is enclosed by the loop
 			if count%2 != 0 {
 				result++
 			}
@@ -54,7 +54,7 @@ func castRays(matrix []string, loop []*Node) (result int) {
 	return result
 }
 
-// Check if on the loop and if so if on corner
+// Check if a point is on the loop and if so if it's on a corner too
 func onLoop(loc Loc, loop []*Node) (node *Node, onloop bool, oncorner bool) {
 	for i, node := range loop {
 		if node.loc != loc {
