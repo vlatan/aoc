2091e5499ad0fbeffcbc69e3f7b594dbf9a5014c
vlatan <33163699+vlatan@users.noreply.github.com>
2024-09-17 14:07:25 +0200

    Refactor D12

--- 2023/12/part1.go
+++ 2023/12/part1.go
@@ -2,10 +2,7 @@ package day12
 
 import (
 	"fmt"
-	"slices"
 )
-
-type Cache map[string]int
 
 // https://adventofcode.com/2023/day/12
 func Part1() {
@@ -15,76 +12,4 @@ func Part1() {
 		result += solve(lines[i], groups[i], Cache{})
 	}
 	fmt.Println(result)
-}
-
-// Recursive function to branch out on "?"
-// and test two paths ("." and "#")
-// only if those paths are worth considering.
-func solve(s string, gs []int, cache Cache) int {
-	for i := 0; i < len(s); i++ {
-		if s[i] != '?' {
-			continue
-		}
-		result := 0
-		ps := s[:i] + "."
-		if gg := getGroups(ps); viable(gg, gs) {
-			key := s[i+1:] + fmt.Sprintf("%v", gg)
-			if val, ok := cache[key]; ok {
-				result += val
-			} else {
-				result += solve(ps+s[i+1:], gs, cache)
-				cache[key] = result
-			}
-		}
-		ps = s[:i] + "#"
-		if gg := getGroups(ps); viable(gg, gs) {
-			key := s[i+1:] + fmt.Sprintf("%v", gg)
-			if val, ok := cache[key]; ok {
-				result += val
-			} else {
-				result += solve(ps+s[i+1:], gs, cache)
-			}
-		}
-		return result
-	}
-	if slices.Equal(getGroups(s), gs) {
-		return 1
-	}
-	return 0
-}
-
-// Calculate groups of '#' from a string
-func getGroups(s string) []int {
-	groupFound, groups := false, []int{}
-	for i := 0; i < len(s); i++ {
-		switch s[i] {
-		case '#':
-			if groupFound {
-				groups[len(groups)-1]++
-				continue
-			}
-			groupFound = true
-			groups = append(groups, 1)
-		case '.':
-			groupFound = false
-		}
-	}
-	return groups
-}
-
-// Check if a partial string is viable to continue.
-// Has the correct groups so far.
-func viable(g1 []int, g2 []int) bool {
-	if len(g1) > len(g2) {
-		return false
-	}
-	for i := 0; i < len(g1); i++ {
-		if i == len(g1)-1 && g1[i] <= g2[i] {
-			return true
-		}
-		if g1[i] != g2[i] {
-			return false
-		}
-	}
-	return true
 }
--- 2023/12/utils.go
+++ 2023/12/utils.go
@@ -3,9 +3,13 @@ package day12
 import (
 	"aoc/2023/utils"
 	"bufio"
+	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
+
+type Cache map[string]int
 
 func parseFile(path string) ([]string, [][]int) {
 	file, err := os.Open(path)
@@ -25,4 +29,70 @@ func parseFile(path string) ([]string, [][]int) {
 		groups = append(groups, group)
 	}
 	return lines, groups
+}
+
+// Recursive function to branch out on "?"
+// and test two paths ("." and "#")
+// only if those paths are not in cache and worth considering.
+func solve(s string, gs []int, cache Cache) int {
+	for i := 0; i < len(s); i++ {
+		if s[i] != '?' {
+			continue
+		}
+		result := 0
+		ps := s[:i] + "."
+		gg := getGroups(ps)
+		key := s[i+1:] + fmt.Sprintf("%v", gg)
+		if val, ok := cache[key]; ok {
+			result += val
+		} else if viable(gg, gs) {
+			result += solve(ps+s[i+1:], gs, cache)
+			cache[key] = result
+		}
+		ps = s[:i] + "#"
+		if gg := getGroups(ps); viable(gg, gs) {
+			result += solve(ps+s[i+1:], gs, cache)
+		}
+		return result
+	}
+	if slices.Equal(getGroups(s), gs) {
+		return 1
+	}
+	return 0
+}
+
+// Calculate groups of '#' from a string
+func getGroups(s string) []int {
+	groupFound, groups := false, []int{}
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '#':
+			if groupFound {
+				groups[len(groups)-1]++
+				continue
+			}
+			groupFound = true
+			groups = append(groups, 1)
+		case '.':
+			groupFound = false
+		}
+	}
+	return groups
+}
+
+// Check if a partial string is viable to continue.
+// Has the correct groups so far.
+func viable(g1 []int, g2 []int) bool {
+	if len(g1) > len(g2) {
+		return false
+	}
+	for i := 0; i < len(g1); i++ {
+		if i == len(g1)-1 && g1[i] <= g2[i] {
+			return true
+		}
+		if g1[i] != g2[i] {
+			return false
+		}
+	}
+	return true
 }
