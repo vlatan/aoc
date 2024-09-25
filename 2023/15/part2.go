2fce7699be8c8e8e950277f9c837f71a4fd60ba2
vlatan <33163699+vlatan@users.noreply.github.com>
2024-09-25 15:16:44 +0200

    Refactor D15P2

--- 2023/15/part2.go
+++ 2023/15/part2.go
@@ -7,47 +7,53 @@ type Label struct {
 	focus int
 }
 
+type Box []Label
+
 // https://adventofcode.com/2023/day/15
 func Part2() {
 	data := parseFile("15/input.txt")
-	var hashmap [256][]Label
+	var hashmap [256]Box
 
 	for _, item := range data {
-
 		switch item[len(item)-1] {
 		case '-':
 			name := item[:len(item)-1]
-			box := hash(name)
-			for i, l := range hashmap[box] {
-				if l.name == name {
-					hashmap[box] = append(hashmap[box][:i], hashmap[box][i+1:]...)
-					break
-				}
-			}
+			hashmap[hash(name)].Remove(name)
 		default:
-			box := hash(item[:len(item)-2])
-			label := Label{item[:len(item)-2], int(item[len(item)-1] - '0')}
-			found := false
-			for i, l := range hashmap[box] {
-				if l.name == label.name {
-					hashmap[box][i] = label
-					found = true
-					break
-				}
-			}
-			if !found {
-				hashmap[box] = append(hashmap[box], label)
-			}
-
+			name := item[:len(item)-2]
+			focus := int(item[len(item)-1] - '0')
+			label := Label{name, focus}
+			hashmap[hash(name)].Replace(label)
 		}
 	}
 
-	result := 0
+	fmt.Println(focusPower(hashmap))
+}
+
+func (b *Box) Remove(name string) {
+	for i, label := range *b {
+		if label.name == name {
+			*b = append((*b)[:i], (*b)[i+1:]...)
+			return
+		}
+	}
+}
+
+func (b *Box) Replace(label Label) {
+	for i, l := range *b {
+		if l.name == label.name {
+			(*b)[i] = label
+			return
+		}
+	}
+	*b = append(*b, label)
+}
+
+func focusPower(hashmap [256]Box) (r int) {
 	for i, box := range hashmap {
 		for j, label := range box {
-			result += (i + 1) * (j + 1) * label.focus
+			r += (i + 1) * (j + 1) * label.focus
 		}
 	}
-
-	fmt.Println(result)
+	return
 }
