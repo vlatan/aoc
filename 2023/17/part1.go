73223593469463163c850b4d83cce3d40f6739a7
vlatan <33163699+vlatan@users.noreply.github.com>
2024-10-09 02:46:42 +0200

    Use struct for location

--- 2023/17/part1.go
+++ 2023/17/part1.go
@@ -6,29 +6,32 @@ import (
 )
 
 type P struct{ x, y int }
-type State struct{ x, y, dx, dy, streak, loss int }
+type State struct {
+	loc                  P
+	dx, dy, streak, loss int
+}
 
 // https://adventofcode.com/2023/day/17
 func Part1() {
 	m := parseFile("17/input.txt")
-	result := solve(m, P{0, 0}, P{len(m) - 1, len(m[0]) - 1})
+	start, end := P{0, 0}, P{len(m) - 1, len(m[0]) - 1}
+	result := solve(m, start, end)
 	fmt.Println(result)
 }
 
 func solve(m Matrix, start, end P) int {
 	visited := make(map[State]struct{})
-	x, y := start.x, start.y
 	pq := &PriorityQueue{}
 	heap.Init(pq)
-	heap.Push(pq, State{x, y, 1, 0, 0, 0})
-	heap.Push(pq, State{x, y, 0, 1, 0, 0})
+	heap.Push(pq, State{start, 1, 0, 0, 0})
+	heap.Push(pq, State{start, 0, 1, 0, 0})
 
 	for pq.Len() > 0 {
 		// pop the state with the least accumulated heat loss
 		state := heap.Pop(pq).(State)
 
 		// if end state
-		if (P{state.x, state.y}) == end {
+		if state.loc == end {
 			return state.loss
 		}
 
@@ -36,7 +39,7 @@ func solve(m Matrix, start, end P) int {
 		for _, n := range getNextStates(m, state) {
 			if _, ok := visited[n]; !ok {
 				visited[n] = struct{}{}
-				n.loss = state.loss + m[n.x][n.y]
+				n.loss = state.loss + m[n.loc.x][n.loc.y]
 				heap.Push(pq, n)
 			}
 		}
@@ -48,21 +51,21 @@ func getNextStates(m Matrix, s State) (r []State) {
 	xMax, yMax := len(m)-1, len(m[0])-1
 
 	dx, dy := s.dy, -s.dx
-	x, y := s.x+dx, s.y+dy
+	x, y := s.loc.x+dx, s.loc.y+dy
 	if inBounds(xMax, yMax, x, y) {
-		r = append(r, State{x, y, dx, dy, 1, m[x][y]})
+		r = append(r, State{P{x, y}, dx, dy, 1, m[x][y]})
 	}
 
 	dx, dy = -s.dy, s.dx
-	x, y = s.x+dx, s.y+dy
+	x, y = s.loc.x+dx, s.loc.y+dy
 	if inBounds(xMax, yMax, x, y) {
-		r = append(r, State{x, y, dx, dy, 1, m[x][y]})
+		r = append(r, State{P{x, y}, dx, dy, 1, m[x][y]})
 	}
 
 	dx, dy = s.dx, s.dy
-	x, y = s.x+dx, s.y+dy
+	x, y = s.loc.x+dx, s.loc.y+dy
 	if inBounds(xMax, yMax, x, y) && s.streak < 3 {
-		r = append(r, State{x, y, dx, dy, s.streak + 1, m[x][y]})
+		r = append(r, State{P{x, y}, dx, dy, s.streak + 1, m[x][y]})
 	}
 
 	return
