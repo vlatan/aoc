05b13ff7c8edc8a449e8c0d84efdafd271312040
vlatan <33163699+vlatan@users.noreply.github.com>
2024-10-09 03:32:19 +0200

    Remove func, add comments

--- 2023/17/part1.go
+++ 2023/17/part1.go
@@ -31,16 +31,19 @@ func solve(m Matrix, start, end P) int {
 		// pop the state with the least accumulated heat loss
 		state := heap.Pop(pq).(State)
 
-		// if end state
+		// if end location we're done
 		if state.loc == end {
 			return state.loss
 		}
 
 		// create the next states
-		for _, n := range state.CreateNewStates(m) {
+		for _, n := range state.CreateNextStates(m) {
+			// if state not processed
 			if _, ok := visited[n]; !ok {
 				visited[n] = struct{}{}
-				n.loss = state.loss + m[n.loc.x][n.loc.y]
+				// compound the heat loss for this state
+				n.loss += state.loss
+				// push the state to heap to be processed
 				heap.Push(pq, n)
 			}
 		}
@@ -48,25 +51,21 @@ func solve(m Matrix, start, end P) int {
 	return 0
 }
 
-func (s State) CreateNewStates(m Matrix) (r States) {
+func (s State) CreateNextStates(m Matrix) (r States) {
+	// turn left/right or up/down
 	r.Push(s.dy, -s.dx, 1, s.loc, m)
 	r.Push(-s.dy, s.dx, 1, s.loc, m)
+	// continue forward/backward if allowed
 	if s.streak < 3 {
 		r.Push(s.dx, s.dy, s.streak+1, s.loc, m)
 	}
-
 	return
 }
 
-func inBounds(xMax, yMax, x, y int) bool {
-	return x >= 0 && x <= xMax &&
-		y >= 0 && y <= yMax
-}
-
 func (r *States) Push(dx, dy, streak int, loc P, m Matrix) {
-	xMax, yMax := len(m)-1, len(m[0])-1
 	x, y := loc.x+dx, loc.y+dy
-	if inBounds(xMax, yMax, x, y) {
+	// if in bounds append new state to slice
+	if x >= 0 && x < len(m) && y >= 0 && y < len(m[0]) {
 		*r = append(*r, State{P{x, y}, dx, dy, streak, m[x][y]})
 	}
 }
